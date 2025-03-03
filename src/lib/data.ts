7f6d7cbf077faeabfe749ec551d94f61dc5b5050
MAC <mac@Allenade-MacBook-Air.local>
2025-03-03 13:13:00 +0100

    updated

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,26 +1,25 @@
 export const projects = [
   {
     id: 1,
-    category: "WEB3",
-    title: "Guild Protocol",
+    category: "Ai",
+    title: "",
     description: "Landing Page for a play to earn crypto DAO",
-    technologies: ["NEXTJS", "TS", "GSAP"],
-    liveLink: "#", // Replace with actual URL
+    technologies: ["NEXTJS", "TS", "Tailwind"],
+    liveLink: "https://github.com/Allenade/learn-wih-ai", // Replace with actual URL
   },
   {
     id: 2,
-    category: "AI",
-    title: "AI Chatbot",
+    category: "E-commerce",
+    title: "E-commerce",
     description: "An AI-powered chatbot built with Next.js and OpenAI API",
-    technologies: ["NEXTJS", "TS", "Tailwind"],
-    liveLink: "#",
+    technologies: ["ReactJS", "Sass"],
+    liveLink: "https://github.com/Allenade/eshop",
   },
   {
     id: 3,
-    category: "Finance",
-    title: "Crypto Dashboard",
-    description: "A dashboard to track real-time cryptocurrency prices.",
+    category: "AI",
+    title: "Nubia Ai",
     technologies: ["NEXTJS", "React", "Tailwind"],
-    liveLink: "#",
+    liveLink: "https://www.nubia.ai/",
   },
 ];
